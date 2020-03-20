063c0ee008b148342b4f639da87f645162f2f75b
Dave Ballard <e.dave.ballard@gmail.com>
2020-03-20 09:53:56 -0500

    Markov upgrade
    Reworked markov to handle tweets better and determine its own end-of-sentence

--- Python/Demos/markov.py
+++ Python/Demos/markov.py
@@ -1,18 +1,42 @@
-import string
 import random
 
 #reading the file, input.txt in same dir
-f = open("input.txt", "r")
+f = open("tweets.txt", "r", errors='ignore')
 lines = f.readlines()
 #place to put words
 words = {}
 last_word = ""
 
+skips = ["http","@","#"]
+ends = [".","?","!"]
+
+#line.translate(str.maketrans('', '', string.punctuation))
+
 #doing this line-by-line
 #so long documents work
 for line in lines:
-   for word in line.translate(str.maketrans('', '', string.punctuation)).lower().strip().split():
-        #line 1
+   for word in line.translate(str.maketrans('', '', '"$%&\'()*+,-/:;<=>[\\]^_`{|}~â€œ™¦‡ºðÿ”')).lower().strip().split():
+        end_of_line = False
+        
+        #check for end of sentence
+        for e in ends:
+            if e in word:
+                end_of_line = True
+                word = word.replace(e,"")
+        
+        #skip urls, hashtags, twitter handles
+        skipword = False
+        for s in skips:
+            if s in word:
+                skipword = True
+                break
+        if skipword:
+            continue
+
+        
+        
+
+        #link words
         if last_word == "":
             last_word = word
             continue
@@ -22,16 +46,33 @@ for line in lines:
             words[last_word][word] = 0
         words[last_word][word] += 1
         last_word = word
+        
+        #make end_of_line possible
+        if(end_of_line):
+            if last_word not in words:
+                words[last_word] = {}
+            if "EOL" not in words[last_word]:
+                words[last_word]["EOL"] = 0
+            words[last_word]["EOL"] += 1
+            
 
 #print(words)
 
 user_in = ""  
 while user_in != "quit":
-    user_in = input("length of chain: ")
-    current_word = random.choice(list(words.keys()))
-    sentence = current_word
+    user_in = input("First word (blank for random): ").lower()
+    if(user_in == ""):
+        current_word = random.choice(list(words.keys()))
+    elif(user_in not in list(words.keys())):
+        print("Word not found.")
+        continue
+    else:
+        current_word = user_in
+        
+        
+    sentence = current_word[0].upper() + current_word[1:]
     #do n words
-    for i in range(int(user_in)):
+    while(current_word != "EOL"):
         #count up the total incidences
         sum = 0
         for k in list(words[current_word].keys()):
@@ -43,6 +84,9 @@ while user_in != "quit":
             choice -= words[current_word][k]
             if choice <= 0:
                 current_word = k
-                sentence += " " + current_word
                 break
+        if current_word == "EOL":
+            sentence += random.choice(ends)
+        else:
+            sentence += " " + current_word
     print(sentence)
